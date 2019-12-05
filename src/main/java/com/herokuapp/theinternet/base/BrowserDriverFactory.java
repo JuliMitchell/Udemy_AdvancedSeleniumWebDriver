0296f6b6729ee999472d9759ae751c449ebc78d4
Mitchell <jmitchell@grupoassa.com>
2019-12-05 10:12:43 -0300

    Adding Log4j class in BaseTest and BrowserDriverFactory

--- src/main/java/com/herokuapp/theinternet/base/BrowserDriverFactory.java
+++ src/main/java/com/herokuapp/theinternet/base/BrowserDriverFactory.java
@@ -1,5 +1,6 @@
 package com.herokuapp.theinternet.base;
 
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -8,9 +9,11 @@ public class BrowserDriverFactory {
 
     private ThreadLocal<WebDriver> driver = new ThreadLocal<WebDriver>();
     private String browser;
+    private Logger log;
 
-    public BrowserDriverFactory(String browser){
+    public BrowserDriverFactory(String browser, Logger log){
         this.browser = browser.toLowerCase();
+        this.log = log;
     }
 
 
